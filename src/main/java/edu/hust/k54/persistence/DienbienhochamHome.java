533f408b260f632952c1f75c2364aa00992a8be1
Nguyen Vi Duong (vi.duong.bk@gmail.com) <Nguyen Vi Duong (vi.duong.bk@gmail.com)@duongnv-PC>
2012-12-15 22:42:12 +0700

    fix hibernate and test controller ok

--- src/main/java/edu/hust/k54/persistence/DienbienhochamHome.java
+++ src/main/java/edu/hust/k54/persistence/DienbienhochamHome.java
@@ -1,6 +1,5 @@
 package edu.hust.k54.persistence;
 
-// Generated Nov 26, 2012 7:17:42 PM by Hibernate Tools 4.0.0
 
 import java.util.List;
 import javax.naming.InitialContext;
@@ -10,18 +9,13 @@ import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Example;
 
-/**
- * Home object for domain model class Dienbienhocham.
- * @see edu.hust.k54.persistence.Dienbienhocham
- * @author Hibernate Tools
- */
 public class DienbienhochamHome {
 
 	private static final Log log = LogFactory.getLog(DienbienhochamHome.class);
 
 	private final SessionFactory sessionFactory = getSessionFactory();
 
-	protected SessionFactory getSessionFactory() {
+	public SessionFactory getSessionFactory() {
 		try {
 			return (SessionFactory) new InitialContext()
 					.lookup("SessionFactory");
--- src/main/java/edu/hust/k54/persistence/DienbienhocviHome.java
+++ src/main/java/edu/hust/k54/persistence/DienbienhocviHome.java
@@ -1,6 +1,5 @@
 package edu.hust.k54.persistence;
 
-// Generated Nov 26, 2012 7:17:42 PM by Hibernate Tools 4.0.0
 
 import java.util.List;
 import javax.naming.InitialContext;
@@ -10,11 +9,6 @@ import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Example;
 
-/**
- * Home object for domain model class Dienbienhocvi.
- * @see edu.hust.k54.persistence.Dienbienhocvi
- * @author Hibernate Tools
- */
 public class DienbienhocviHome {
 
 	private static final Log log = LogFactory.getLog(DienbienhocviHome.class);
