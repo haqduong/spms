84adef8e8090d258d222ed03eb4bb3fbe100bd59
Nguyen Vi Duong (vi.duong.bk@gmail.com) <Nguyen Vi Duong (vi.duong.bk@gmail.com)@duongnv-PC>
2012-12-17 23:26:39 +0700

    add getLowerPermission into Manager.java

--- src/main/java/edu/hust/k54/controller/Manager.java
+++ src/main/java/edu/hust/k54/controller/Manager.java
@@ -1,4 +1,6 @@
 package edu.hust.k54.controller;
+import java.util.Set;
+
 import edu.hust.k54.persistence.Donviquanly;
 import edu.hust.k54.persistence.DonviquanlyHome;
 import edu.hust.k54.persistence.Hesoluong;
@@ -9,11 +11,51 @@ import edu.hust.k54.persistence.Kyluat;
 import edu.hust.k54.persistence.KyluatHome;
 import edu.hust.k54.persistence.Phongban;
 import edu.hust.k54.persistence.PhongbanHome;
+import edu.hust.k54.persistence.Soyeulylich;
+import edu.hust.k54.persistence.SoyeulylichHome;
 import edu.hust.k54.persistence.Taikhoandangnhap;
 import edu.hust.k54.persistence.TaikhoandangnhapHome;
 
 
 public class Manager {
+	private static final Integer STAFF_PERMISSION = 1;
+	private static final Integer MANAGER_PERMISSION = 2;
+	private static final Integer SUPER_MANAGER_PERMISSION = 3;
+	
+	public Set<Soyeulylich> getLowerPermission(Integer idcanbo){
+		Set<Soyeulylich> output = null;
+		SoyeulylichHome soyeulylichHome = new SoyeulylichHome();
+		Soyeulylich soyeulylich =  soyeulylichHome.findById(idcanbo);
+		if(soyeulylich != null){
+			Taikhoandangnhap taikhoandangnhap = (Taikhoandangnhap)soyeulylich.getTaikhoandangnhaps().iterator().next();
+			Integer currenPermission = taikhoandangnhap.getPermission();
+			if(currenPermission == STAFF_PERMISSION){
+				return null; //staff
+			}else if(currenPermission == MANAGER_PERMISSION){
+				Phongban phongban = soyeulylich.getPhongban();
+				Set<Soyeulylich> listStaff = phongban.getSoyeulyliches();
+				for (Soyeulylich staff : listStaff) {
+					if(((Taikhoandangnhap)staff.getTaikhoandangnhaps().iterator().next()).getPermission() < currenPermission){
+						output.add(staff);
+					}
+				}
+				return output;
+			}else if(currenPermission == SUPER_MANAGER_PERMISSION){
+				Donviquanly donviquanly = soyeulylich.getDonviquanly();
+				Set<Phongban> listPhongBan = donviquanly.getPhongbans();
+				for (Phongban phongban : listPhongBan) {
+					Set<Soyeulylich> listStaff = phongban.getSoyeulyliches();
+					for (Soyeulylich staff : listStaff) {
+						if(((Taikhoandangnhap)staff.getTaikhoandangnhaps().iterator().next()).getPermission() < currenPermission){
+							output.add(staff);
+						}
+					}
+				}
+				return output;
+			}
+		}
+		return null;
+	}
 	
 	public void capNhatTTPB(Phongban phongban){
 		PhongbanHome phongbanHome = new PhongbanHome();
